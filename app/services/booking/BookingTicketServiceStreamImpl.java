9b49abf7bb12a19405c058c51a5882db460ede94
Spider man <nguyendo94vn@gmail.com>
2018-03-19 10:52:03 +0700

    update example for merge 2 tasks

--- app/services/booking/BookingTicketServiceStreamImpl.java
+++ app/services/booking/BookingTicketServiceStreamImpl.java
@@ -1,8 +1,9 @@
 package services.booking;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
-import akka.stream.ActorMaterializer;
-import akka.stream.Materializer;
+import akka.japi.function.Function;
+import akka.stream.*;
 import akka.stream.javadsl.*;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.Inject;
@@ -13,7 +14,6 @@ import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import play.libs.Json;
 
-import java.util.ArrayList;
 import java.util.concurrent.CompletionStage;
 
 public class BookingTicketServiceStreamImpl implements BookingService<CompletionStage<ObjectNode>> {
@@ -27,21 +27,65 @@ public class BookingTicketServiceStreamImpl implements BookingService<Completion
 
     @Override
     public CompletionStage<ObjectNode> bookingTicket() {
-        return Source.combine(
-                Source.fromPublisher(findTicketFlight()),
-                Source.fromPublisher(findTicketHotel()),
-                new ArrayList<>(), Merge::create)
-                .runWith(Sink.reduce((ObjectNode ticketFlight, ObjectNode ticketHotel) -> {
-                    ObjectNode data = Json.newObject();
-                    data.putPOJO("flight", ticketFlight);
-                    data.putPOJO("hotel", ticketHotel);
-                    return data;
-                }), mat).toCompletableFuture();
+
+        Flow<ObjectNode, ObjectNode, NotUsed> f1 = Flow.of(ObjectNode.class).map(elem -> elem).async();
+        Flow<ObjectNode, ObjectNode, NotUsed> f2 = Flow.of(ObjectNode.class).map(elem -> elem);
+        Flow<ObjectNode, ObjectNode, NotUsed> f3 = Flow.of(ObjectNode.class).map(elem -> elem);
+        Flow<ObjectNode, ObjectNode, NotUsed> f4 = Flow.of(ObjectNode.class).map(elem -> elem).async();
+        Flow<ObjectNode, ObjectNode, NotUsed> f5 = Flow.of(ObjectNode.class).map(elem -> elem);
+
+        Source<ObjectNode, NotUsed> inFlight = Source.fromPublisher(findTicketFlight());
+        Source<ObjectNode, NotUsed> inHotel = Source.fromPublisher(findTicketHotel());
+
+        Sink<ObjectNode, CompletionStage<ObjectNode>> sink = Sink.head();
+
+        RunnableGraph<CompletionStage<ObjectNode>> graph =
+                RunnableGraph.fromGraph(
+                        GraphDSL.create(sink, (builder, out) -> {
+
+                            final UniformFanOutShape<ObjectNode, ObjectNode> bcastFlight = builder.add(Broadcast.create(1));
+                            final UniformFanOutShape<ObjectNode, ObjectNode> bcastHotel = builder.add(Broadcast.create(1));
+
+                            final UniformFanInShape<ObjectNode, ObjectNode> merge = builder.add(Merge.create(2));
+
+                            final Outlet<ObjectNode> sourceFlight = builder.add(inFlight).out();
+                            final Outlet<ObjectNode> sourceHotel = builder.add(inHotel).out();
+
+                            builder.from(sourceFlight).via(builder.add(f1)).
+                                    viaFanOut(bcastFlight)
+                                    .via(builder.add(f2)).viaFanIn(merge).via(builder.add(f3)).to(out);
+
+                            builder.from(sourceHotel).via(builder.add(f4))
+                                    .viaFanOut(bcastHotel)
+                                    .via(builder.add(f5)).viaFanIn(merge);
+
+                            return ClosedShape.getInstance();
+                        }));
+        return graph.run(mat);
     }
+
+//        return Source.combine(
+//                Source.fromPublisher(findTicketFlight()),
+//                Source.fromPublisher(findTicketHotel()),
+//                new ArrayList<>(), Merge::create)
+//                .runWith(Sink.reduce((ObjectNode ticketFlight, ObjectNode ticketHotel) -> {
+//                    ObjectNode data = Json.newObject();
+//                    data.putPOJO("flight", ticketFlight);
+//                    data.putPOJO("hotel", ticketHotel);
+//                    return data;
+//                }).async(), mat).toCompletableFuture();
 
     private Publisher<ObjectNode> findTicketHotel() {
         return (Subscriber<? super ObjectNode> s) -> {
+            System.out.println("+++++++++++++++ findTicketHotel");
             Ticket ticketHotel = TicketHotel.db().createQuery(TicketHotel.class).findList().get(0);
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             s.onNext((ObjectNode)Json.toJson(ticketHotel));
             s.onComplete();
         };
@@ -49,7 +93,15 @@ public class BookingTicketServiceStreamImpl implements BookingService<Completion
 
     private Publisher<ObjectNode> findTicketFlight() {
         return (Subscriber<? super ObjectNode> s) -> {
+            System.out.println("+++++++++++++++ findTicketFlight");
             Ticket ticketFlight = TicketFlight.db().createQuery(TicketFlight.class).findList().get(0);
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             s.onNext((ObjectNode) Json.toJson(ticketFlight));
             s.onComplete();
         };
